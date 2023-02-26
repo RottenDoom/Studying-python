175c425a94bd4c9a4751ef61ee764dd259002c25
RottenDoom <123873439+RottenDoom@users.noreply.github.com>
2023-02-26 23:01:13 +0530

    conditionals and yeah i am gonna start for loops really soon .

---
+++ listsandconditionals.py
@@ -0,0 +1,6 @@
+# In this his code we will study about lists and more about conditionals.
+# Lists are handy objects and are iterables .
+# The if statements always goes from top to bottom and gives the output for the first conditional-
+# -it meets in its way which is true.
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+print([x for x in a if x < 5])
