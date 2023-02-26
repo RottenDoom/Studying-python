b934d3e3db4f27b29152c769756f639cfb8a7fb0
RottenDoom <123873439+RottenDoom@users.noreply.github.com>
2023-02-26 18:52:54 +0530

    This is me studying python

---
+++ INPUTTUT.py
@@ -0,0 +1,4 @@
+# Here I am going to make a program so that ican find the year that a guy turns 100 years old.
+name = input("What is your name :")
+year = int(input("Your birth year :"))
+print("When you turn 100 year old " + name + " that year will be " + str(year + 100))
