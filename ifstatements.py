87683538c5426f101a51f81ea290f9d6aec5a815
RottenDoom <123873439+RottenDoom@users.noreply.github.com>
2023-02-26 21:58:01 +0530

    This is me learning if statements.

---
+++ ifstatements.py
@@ -0,0 +1,7 @@
+# Here I am going to practice if statement along with modulo statements.
+Num = int(input("Enter your number :"))
+x = Num % 2
+if x == 0:
+    print("Even")
+else:
+    print("Odd")
