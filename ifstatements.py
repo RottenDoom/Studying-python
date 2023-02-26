e770b897b5e1e3bf727c14275e7eb05b69003953
RottenDoom <123873439+RottenDoom@users.noreply.github.com>
2023-02-26 22:23:53 +0530

    added the multiple of 4 thingy and yeah modulo is pretty handy yeah!

--- ifstatements.py
+++ ifstatements.py
@@ -1,7 +1,13 @@
 # Here I am going to practice if statement along with modulo statements.
 Num = int(input("Enter your number :"))
 x = Num % 2
+y = Num % 4
 if x == 0:
     print("Even")
 else:
     print("Odd")
+
+if y == 0:
+    print("Wow this number is a multiple of 4")
+else:
+    print("Oof this number sucks!!!")
